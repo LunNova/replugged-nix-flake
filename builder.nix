75de1d37b154168b1d0691cedfc627b678b307ba
Luna Nova <git@lunnova.dev>
2022-08-07 14:47:47 -0700

    chore: run nixpkgs-fmt

--- builder.nix
+++ builder.nix
@@ -1,20 +1,21 @@
-{ pkgs, replugged-src, themes ? {}, plugins ? {}, extraElectronArgs ? "" }:
-let self = 
-{
-  replugged-unwrapped = pkgs.callPackage ./drvs/replugged-unwrapped.nix {
-    inherit replugged-src;
-  };
+{ pkgs, replugged-src, themes ? { }, plugins ? { }, extraElectronArgs ? "" }:
+let
+  self =
+    {
+      replugged-unwrapped = pkgs.callPackage ./drvs/replugged-unwrapped.nix {
+        inherit replugged-src;
+      };
 
-  replugged = pkgs.callPackage ./drvs/replugged.nix {
-   inherit (self) replugged-unwrapped;
-   withPlugins = plugins;
-   withThemes = themes;
-  };
+      replugged = pkgs.callPackage ./drvs/replugged.nix {
+        inherit (self) replugged-unwrapped;
+        withPlugins = plugins;
+        withThemes = themes;
+      };
 
-  discord-plugged = pkgs.callPackage ./drvs/discord-plugged.nix {
-    inherit extraElectronArgs;
-    inherit (self) replugged;
-  };
-};
+      discord-plugged = pkgs.callPackage ./drvs/discord-plugged.nix {
+        inherit extraElectronArgs;
+        inherit (self) replugged;
+      };
+    };
 in
 self
--- flake.nix
+++ flake.nix
@@ -8,21 +8,30 @@
   };
 
   outputs = { self, nixpkgs, ... } @ inputs:
-  let
-    replugged-src = inputs.replugged;
-    builder = import ./builder.nix;
-    forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
-  in {
-    lib = let self = {
-      makeDiscordPluggedPackageSet = builder-args: builder { inherit replugged-src; } // builder-args;
-      makeDiscordPlugged = args: (self.makeDiscordPluggedPackageSet args).discord-plugged;
-    }; in self;
-    overlay = pkgs: prev: (builder {
-      inherit pkgs replugged-src;
-    });
-    packages = forAllSystems (system: let pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    }; in builder { inherit pkgs replugged-src; });
-  };
+    let
+      replugged-src = inputs.replugged;
+      builder = import ./builder.nix;
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in
+    {
+      lib =
+        let
+          self = {
+            makeDiscordPluggedPackageSet = builder-args: builder { inherit replugged-src; } // builder-args;
+            makeDiscordPlugged = args: (self.makeDiscordPluggedPackageSet args).discord-plugged;
+          };
+        in
+        self;
+      overlay = pkgs: prev: (builder {
+        inherit pkgs replugged-src;
+      });
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in
+        builder { inherit pkgs replugged-src; });
+    };
 }
