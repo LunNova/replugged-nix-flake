420f0c8e99c1dd49458a8397129d4a513c2fbb4c
Luna Nova <git@lunnova.dev>
2022-10-01 12:30:14 -0700

    discord-plugged: add withOpenAsar option which uses https://openasar.dev/

--- builder.nix
+++ builder.nix
@@ -1,7 +1,16 @@
 # Either builds a self contained package set, or can be an overlay if you set the overlayFinal arg
 # in which case it uses the overlay's fixed point and not its own built in one
 # discord arg is the discord package you want to add replugged to, defaults to pkgs.discord
-{ pkgs, replugged-src, discord ? null, themes ? { }, plugins ? { }, extraElectronArgs ? "", discordPathSuffix ? "", overlayFinal ? null }:
+{ pkgs
+, replugged-src
+, discord ? null
+, themes ? { }
+, plugins ? { }
+, extraElectronArgs ? ""
+, discordPathSuffix ? ""
+, overlayFinal ? null
+, withOpenAsar ? false
+}:
 let
   overlayFinalOrSelf = if (overlayFinal == null) then self else overlayFinal;
   self =
@@ -17,7 +26,7 @@ let
       };
 
       discord-plugged = pkgs.callPackage ./drvs/discord-plugged.nix {
-        inherit extraElectronArgs discordPathSuffix;
+        inherit extraElectronArgs discordPathSuffix withOpenAsar;
         inherit (overlayFinalOrSelf) replugged;
         discord = if discord != null then discord else pkgs.discord;
       };
--- drvs/discord-plugged.nix
+++ drvs/discord-plugged.nix
@@ -1,4 +1,5 @@
-{ symlinkJoin
+{ lib
+, symlinkJoin
   # This should usually be discord-${discordPathSuffix} but other versions can work
 , discord
 , replugged
@@ -6,11 +7,17 @@
 , writeShellScript
 , extraElectronArgs ? ""
 , discordPathSuffix ? ""
+, withOpenAsar ? false
 }:
 let
   extractCmd = makeBinaryWrapper.extractCmd or (writeShellScript "extract-binary-wrapper-cmd" ''
     strings -dw "$1" | sed -n '/^makeCWrapper/,/^$/ p'
   '');
+  openAsar = builtins.fetchurl {
+    name = "app.asar";
+    url = "https://objects.githubusercontent.com/github-production-release-asset-2e65be/436587361/528ec0c9-da3d-42d7-8fe6-4078e47e128c?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20221001%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20221001T192103Z&X-Amz-Expires=300&X-Amz-Signature=8c5363c1c99e9f32b99a205783cea3cd68938b2bc46bf98d3dab86173588c1bd&X-Amz-SignedHeaders=host&actor_id=782440&key_id=0&repo_id=436587361&response-content-disposition=attachment%3B%20filename%3Dapp.asar&response-content-type=application%2Foctet-stream";
+    sha256 = "sha256:173wc5q3wqbx5p2qf5yvhgbwpds1v9v266285hzjv39r7zkb531k";
+  };
 in
 symlinkJoin {
   name = "discord-plugged";
@@ -43,7 +50,10 @@ symlinkJoin {
     fi
 
     substituteInPlace $out/opt/Discord${discordPathSuffix}/Discord${discordPathSuffix} --replace '${discord.out}' "$out"
-  '';
+  '' + (lib.optionalString withOpenAsar ''
+    rm $out/opt/Discord${discordPathSuffix}/resources/app.asar
+    cp ${openAsar} $out/opt/Discord${discordPathSuffix}/resources/app.asar
+  '');
 
   meta.mainProgram = if (discord.meta ? mainProgram) then discord.meta.mainProgram else null;
 }
