9161a1ec4f4a15148258ea5a8457bd1930512b7c
Luna Nova <git@lunnova.dev>
2022-10-06 17:46:04 -0700

    discord-plugged: fix openasar url

--- drvs/discord-plugged.nix
+++ drvs/discord-plugged.nix
@@ -15,8 +15,8 @@ let
   '');
   openAsar = builtins.fetchurl {
     name = "app.asar";
-    url = "https://objects.githubusercontent.com/github-production-release-asset-2e65be/436587361/528ec0c9-da3d-42d7-8fe6-4078e47e128c?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20221001%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20221001T192103Z&X-Amz-Expires=300&X-Amz-Signature=8c5363c1c99e9f32b99a205783cea3cd68938b2bc46bf98d3dab86173588c1bd&X-Amz-SignedHeaders=host&actor_id=782440&key_id=0&repo_id=436587361&response-content-disposition=attachment%3B%20filename%3Dapp.asar&response-content-type=application%2Foctet-stream";
-    sha256 = "sha256:173wc5q3wqbx5p2qf5yvhgbwpds1v9v266285hzjv39r7zkb531k";
+    url = "https://github.com/LunNova/replugged-nix-flake/raw/openasar/app.asar";
+    sha256 = "sha256:0mkvmy9fhqmj7z3lfrm38a5nf62s251da9957sgfj9nw23ydqmlp";
   };
 in
 symlinkJoin {
